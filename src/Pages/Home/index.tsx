1ed7179e1627934277a497036eb4277dd68b91cd
Vinicius Dalmazzo de Camargo <vinicius.camargo@neobpo.com.br>
2022-05-23 09:13:34 -0300

    Fixed home buttons links

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,17 @@
 import { BackgroundImage, Container, ContainerButtons, ContentInfos } from './styles';
 import HomeBackgroundImg from '../../assets/Images/home-bg.png';
-import {ReactComponent as LinkedInIcon} from '../../assets/Icons/linkedin-icon.svg';
-import {ReactComponent as DownloadIcon } from '../../assets/Icons/download-icon.svg';
+import { ReactComponent as LinkedInIcon } from '../../assets/Icons/linkedin-icon.svg';
+import { ReactComponent as DownloadIcon } from '../../assets/Icons/download-icon.svg';
+import saveAs from 'file-saver';
 
 function Home() {
+
+  const downloadCv = () => {
+    saveAs(
+      process.env.PUBLIC_URL + "/resource/viniciusdalmazzo-cv.pdf",
+      "viniciusdalmazzo-cv.pdf");
+  }
+
   return (
     <Container>
       <ContentInfos>
@@ -11,8 +19,11 @@ function Home() {
         <h1>vinicius dalmazzo</h1>
         <h3>desenvolvedor full stack.</h3>
         <ContainerButtons>
-          <button><LinkedInIcon />LinkedIn</button>
-          <button><DownloadIcon />Currículo</button>
+          <button onClick={() => {
+            window.open('https://www.linkedin.com/in/vinicius-dalmazzo/', '_blank');
+          }}>
+            <LinkedInIcon />LinkedIn</button>
+          <button onClick={downloadCv}><DownloadIcon />Currículo</button>
         </ContainerButtons>
       </ContentInfos>
       <BackgroundImage image={HomeBackgroundImg} />
